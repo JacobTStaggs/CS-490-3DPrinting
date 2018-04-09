0152722e40d79752c5c43ed815a544d64f573fab
unknown <staggs11@marshall.edu>
2018-04-09 04:16:44 -0400

    test

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var configDB = require('./config/database.js');
 mongoose.connect(configDB.url);
 
